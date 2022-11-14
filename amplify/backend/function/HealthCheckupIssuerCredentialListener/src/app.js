87592bd0561044a56996e5997acc452ef785193f
“Takahiro <takahiro-kanuma@gmogshd.com>
2022-11-14 17:37:41 +0900

    label change done.

--- amplify/backend/function/HealthCheckupIssuerCredentialListener/src/app.js
+++ amplify/backend/function/HealthCheckupIssuerCredentialListener/src/app.js
@@ -86,9 +86,9 @@ app.delete("/topic/issue_credential/*", function (req, res) {
   res.json({ success: "delete call succeed!", url: req.url });
 });
 
-app.listen(3000, function () {
-  console.log("App started");
-});
+// app.listen(3000, function () {
+//   console.log("App started");
+// });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
--- src/modules/api/patients.js
+++ src/modules/api/patients.js
@@ -55,14 +55,78 @@ export const changeCheckupResultLabel = (checkupResultRaw) => {
   checkupResultRaw["受診者ID"] = checkupResultRaw.patientId;
   delete checkupResultRaw.patientId;
 
+  checkupResultRaw["名前"] = checkupResultRaw.name;
+  delete checkupResultRaw.name;
+
   checkupResultRaw["年齢"] = checkupResultRaw.age;
   delete checkupResultRaw.age;
+
+  checkupResultRaw["メールアドレス"] = checkupResultRaw.mailAddress;
+  delete checkupResultRaw.mailAddress;
+
+  checkupResultRaw["受診日"] = new Date(
+    checkupResultRaw.checkedupDate * 1000
+  ).toLocaleDateString();
+  delete checkupResultRaw.checkedupDate;
+
+  const issueState = checkupResultRaw.issueState;
+  if (issueState === 0) {
+    checkupResultRaw["証明書発行状態"] = "未発行";
+  } else if (issueState === 1) {
+    checkupResultRaw["証明書発行状態"] = "発行QRコード送信済み";
+  } else if (issueState === 2) {
+    checkupResultRaw["証明書発行状態"] = "発行済み";
+  }
+  delete checkupResultRaw.issueState;
+
+  const issuedDate = checkupResultRaw.issuedDate;
+  if (issuedDate === 0) {
+    checkupResultRaw["証明書発行日"] = null;
+  } else {
+    checkupResultRaw["証明書発行日"] = new Date(
+      checkupResultRaw.checkedupDate * 1000
+    ).toLocaleDateString();
+  }
+  delete checkupResultRaw.issuedDate;
+
+  checkupResultRaw["BMI"] = checkupResultRaw.bmi;
+  delete checkupResultRaw.bmi;
 
   checkupResultRaw["視力"] = checkupResultRaw.eyesight;
   delete checkupResultRaw.eyesight;
 
-  checkupResultRaw["名前"] = checkupResultRaw.name;
-  delete checkupResultRaw.name;
+  checkupResultRaw["聴力"] = checkupResultRaw.hearing;
+  delete checkupResultRaw.hearing;
+
+  checkupResultRaw["腹囲"] = checkupResultRaw.waist;
+  delete checkupResultRaw.waist;
+
+  checkupResultRaw["血圧"] = checkupResultRaw.bloodPressure;
+  delete checkupResultRaw.bloodPressure;
+
+  checkupResultRaw["肺活量"] = checkupResultRaw.vitalCapacity;
+  delete checkupResultRaw.vitalCapacity;
+
+  checkupResultRaw["尿酸値"] = checkupResultRaw.ua;
+  delete checkupResultRaw.ua;
+
+  checkupResultRaw["総コレステロール"] = checkupResultRaw.tc;
+  delete checkupResultRaw.tc;
+
+  checkupResultRaw["中性脂肪"] = checkupResultRaw.tg;
+  delete checkupResultRaw.tg;
+
+  checkupResultRaw["血糖値"] = checkupResultRaw.fpg;
+  delete checkupResultRaw.fpg;
+
+  checkupResultRaw["赤血球"] = checkupResultRaw.rbc;
+  delete checkupResultRaw.rbc;
+
+  checkupResultRaw["白血球"] = checkupResultRaw.wbc;
+  delete checkupResultRaw.wbc;
+
+  checkupResultRaw["血小板数"] = checkupResultRaw.plt;
+  delete checkupResultRaw.plt;
 
   return checkupResultRaw;
 };
