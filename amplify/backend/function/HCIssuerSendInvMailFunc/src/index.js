01e72231ebacd3c3461749b1b10f1a3c098e6d5a
“Takahiro <takahiro-kanuma@gmogshd.com>
2022-12-02 14:29:12 +0900

    bug fix.

--- amplify/backend/function/HCIssuerSendInvMailFunc/src/index.js
+++ amplify/backend/function/HCIssuerSendInvMailFunc/src/index.js
@@ -1,26 +1,51 @@
 const AWS = require("aws-sdk");
 const ses = new AWS.SES({ region: process.env.REGION });
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  const payload = event.responsePayload;
+  const payload = JSON.parse(event.Records[0].body);
+  console.log(payload);
 
-  const sesParams = {
-    Source: process.env.ISSUER_EMAIL_TEST_ADDRESS,
-    Destination: {
-      ToAddresses: [process.env.HOLDER_EMAIL_TEST_ADDRESS],
-    },
-    Message: {
-      Subject: { Data: "健康診断結果証明書発行オファー(仮)" },
-      Body: {
-        Html: {
-          Data: `${payload.patientName}さん</br> <a href='${payload.deepLinkInvitation}'>ここ</a>をクリックして健康診断書証明書を発行してください。(仮)`,
+  try {
+    const sesParams = {
+      Source: process.env.ISSUER_EMAIL_TEST_ADDRESS,
+      Destination: {
+        ToAddresses: [process.env.HOLDER_EMAIL_TEST_ADDRESS],
+      },
+      Message: {
+        Subject: { Data: "健康診断結果証明書発行オファー(仮)" },
+        Body: {
+          Html: {
+            Data: `${payload.patientName}さん</br> <a href='${payload.deepLinkInvitation}'>ここ</a>をクリックして健康診断書証明書を発行してください。(仮)`,
+          },
         },
       },
+    };
+    const sesResp = await ses.sendEmail(sesParams).promise();
+    console.log(JSON.stringify(sesResp));
+  } catch (err) {
+    console.log(`error on sending invitation email to holder: ${err}`);
+    throw Error(err);
+  }
+
+  // DBのissueState更新
+  const paramsforUpdate = {
+    TableName: process.env.STORAGE_PATIENT_NAME,
+    Key: {
+      patientId: payload.patientId,
+    },
+    UpdateExpression: "set issueState = :s",
+    ExpressionAttributeValues: {
+      ":s": 2,
     },
   };
-  await ses.sendEmail(sesParams).promise();
-  return {
-    patientId: payload.patientId,
-  };
+
+  try {
+    const docResp = await docClient.update(paramsforUpdate).promise();
+    console.log(JSON.stringify(docResp));
+  } catch (err) {
+    console.log(`db updating issueState error: ${err}`);
+    throw Error(err);
+  }
 };
