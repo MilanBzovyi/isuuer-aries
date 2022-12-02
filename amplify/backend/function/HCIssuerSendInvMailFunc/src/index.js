890b26308dd365168dadfa70d9646bf209749abe
“Takahiro <takahiro-kanuma@gmogshd.com>
2022-12-02 20:07:00 +0900

    bug fix.

--- amplify/backend/function/HCIssuerSendInvMailFunc/src/index.js
+++ amplify/backend/function/HCIssuerSendInvMailFunc/src/index.js
@@ -24,7 +24,7 @@ exports.handler = async (event) => {
         Subject: { Data: "健康診断結果証明書発行オファー(仮)" },
         Body: {
           Html: {
-            Data: `${payload.patientName}さん</br> <a href='${payload.deepLinkInvitation}'>ここ</a>をクリックして健康診断書証明書を発行してください。(仮)`,
+            Data: `${payload.patientName}さん<br> <a href='${payload.invitation}'>ここ</a>をクリックして健康診断書証明書を発行してください。(仮)`,
           },
         },
       },
