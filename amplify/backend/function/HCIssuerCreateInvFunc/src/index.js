21efc4dc732c1776c676a62587c1ceac477ea0ca
“Takahiro <takahiro-kanuma@gmogshd.com>
2022-12-20 14:40:41 +0900

    bug fix.

--- amplify/backend/function/HCIssuerCreateInvFunc/src/index.js
+++ amplify/backend/function/HCIssuerCreateInvFunc/src/index.js
@@ -1,126 +1,29 @@
-const fetch = require("node-fetch");
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_PATIENT_ARN
+	STORAGE_PATIENT_NAME
+	STORAGE_PATIENT_STREAMARN
+Amplify Params - DO NOT EDIT */
 
 const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
 const sqs = new AWS.SQS();
+
+const fetch = require("node-fetch");
+
 exports.handler = async (event) => {
   console.log(`event: ${JSON.stringify(event)}`);
-  const checkupResult = JSON.parse(event.Records[0].body);
-  console.log(checkupResult);
-
-  // Credential Exchange Record生成のEndpoint呼び出し
-  const issueCrdentialBody = {
-    auto_remove: true,
-    comment: `健康診断書VCの発行 / 受診者ID: ${checkupResult.patientId}`,
-    credential_proposal: {
-      "@type": "issue-credential/1.0/credential-preview",
-      attributes: [
-        {
-          name: "patient_id",
-          value: checkupResult.patientId.toString(),
-        },
-        {
-          name: "bmi",
-          value: checkupResult.bmi.toString(),
-        },
-        {
-          name: "eyesight",
-          value: checkupResult.eyesight.toString(),
-        },
-        {
-          name: "hearing",
-          value: checkupResult.hearing.toString(),
-        },
-        {
-          name: "waist",
-          value: checkupResult.waist.toString(),
-        },
-        {
-          name: "blood_pressure",
-          value: checkupResult.bloodPressure,
-        },
-        {
-          name: "vital_capacity",
-          value: checkupResult.vitalCapacity.toString(),
-        },
-        {
-          name: "ua",
-          value: checkupResult.ua.toString(),
-        },
-        {
-          name: "tc",
-          value: checkupResult.tc.toString(),
-        },
-        {
-          name: "tg",
-          value: checkupResult.tg.toString(),
-        },
-        {
-          name: "fpg",
-          value: checkupResult.fpg.toString(),
-        },
-        {
-          name: "rbc",
-          value: checkupResult.rbc.toString(),
-        },
-        {
-          name: "wbc",
-          value: checkupResult.wbc.toString(),
-        },
-        {
-          name: "plt",
-          value: checkupResult.plt.toString(),
-        },
-      ],
-    },
-    issuer_did: `${process.env.ISSUER_DID}`,
-    schema_id: `${process.env.SCHEMA_ID}`,
-    cred_def_id: `${process.env.CRED_DEF_ID}`,
-    trace: true,
-  };
-
-  const issueCredentialResponse = await fetch(
-    `${process.env.ISSUER_ENDPOINT}/issue-credential/create`,
-    {
-      cache: "no-cache",
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(issueCrdentialBody),
-    }
-  );
-
-  let issueCredentialResJson = null;
-  if (issueCredentialResponse.ok) {
-    issueCredentialResJson = await issueCredentialResponse.json();
-    console.log(JSON.stringify(issueCredentialResJson));
-  } else {
-    const message = "Error on calling aca-py's issue-credential/create";
-    console.error(`${message}: ${issueCredentialResponse.statusText}`);
-    throw Error({
-      matter: message,
-      reason: issueCredentialResponse.statusText,
-    });
-  }
-  const credentialExchangeId = issueCredentialResJson.credential_exchange_id;
-  console.log("Credential Exchange ID", credentialExchangeId);
+  const data = JSON.parse(event.Records[0].body);
+  console.log(data);
 
   // Invitation生成のEndpoint呼び出し
   const createInvitationReqBody = {
-    attachments: [
-      {
-        id: credentialExchangeId,
-        type: "credential-offer",
-      },
-    ],
-    my_label: `${checkupResult.name}さんへのVC発行オファー`,
-    handshake_protocols: [
-      "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0",
-    ],
+    my_label: `${data.name}さんへのInvitation`,
   };
+
   const createInvitationResponse = await fetch(
-    `${process.env.ISSUER_ENDPOINT}/out-of-band/create-invitation`,
+    `${process.env.VERIFIER_ENDPOINT}/connections/create-invitation`,
     {
       cache: "no-cache",
       method: "POST",
@@ -136,7 +39,7 @@ exports.handler = async (event) => {
   if (createInvitationResponse.ok) {
     createInvitationResponseJson = await createInvitationResponse.json();
   } else {
-    const message = "Error on calling aca-py's out-of-band/create-invitation";
+    const message = "Error on calling aca-py's connections/create-invitation";
     console.error(`${message}: ${createInvitationResponse.statusText}`);
     throw new Error({
       matter: message,
@@ -147,15 +50,41 @@ exports.handler = async (event) => {
   const invitationURL = createInvitationResponseJson.invitation_url;
   console.log("invitationURL", invitationURL);
 
+  // applicant idを元にconnection idをDBに保管する。
+  const patientId = data.patientId;
+  const connectionId = createInvitationResponseJson.connectionId;
+
+  const paramsforUpdate = {
+    TableName: process.env.STORAGE_PATIENT_NAME,
+    Key: {
+      patientId: patientId,
+    },
+    UpdateExpression: "set connectionId = :s",
+    ExpressionAttributeValues: {
+      ":s": connectionId,
+    },
+  };
+
+  try {
+    const docResp = await docClient.update(paramsforUpdate).promise();
+    console.log(JSON.stringify(docResp));
+  } catch (err) {
+    console.log(`db updating issueState error: ${err}`);
+    // TODO throw Errorするとメッセージがキュー上から消費されず、永遠と同じことを繰り返してしまう。
+    // TODO return {}するとそれは起きないけど、正常にメッセージが消費されて消える。
+    throw Error(err);
+  }
+
+  // DEEP LINKを作り、QueueにInvitationを投げる。
   const deepLinkInvitation =
-    process.env.INV_FORWARD_URL + invitationURL.split("oob=")[1];
+    process.env.INV_FORWARD_URL + invitationURL.split("c_i=")[1];
   console.log("deepLinkInvitationURL", deepLinkInvitation);
 
   try {
     const sqsParams = {
       MessageBody: JSON.stringify({
-        patientId: checkupResult.patientId,
-        patientName: checkupResult.name,
+        patientId: data.patientId,
+        patientName: data.name,
         invitation: deepLinkInvitation,
       }),
       QueueUrl: process.env.QUEUE_URL,
